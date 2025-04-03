23b769236097df1e1de8e6bc2f8689c8aa81ee43
dtarpein <dtarpein@luc.edu>
2025-04-03 23:08:00 +0000

    :changed http to https in weather API call

--- MinVerse_Arcade/api/external.py
+++ MinVerse_Arcade/api/external.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def get_weather(city):
     api_key = os.getenv('WEATHER_API_KEY')
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={api_key}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={api_key}"
     
     try:
         response = requests.get(url, timeout=10)
